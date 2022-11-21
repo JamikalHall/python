1ed6b66b71f139491a3ba8419c25016f0276ba95
Jamikal <jamikal.hall@gmail.com>
2022-11-21 15:52:57 -0600

    basic algorithm

---
+++ algo-1.py
@@ -0,0 +1,13 @@
+num1 = 0
+num2 = 0
+total = 0
+
+#Create an algorithm that takes two numbers and displays the sum.
+
+def AddNumbers():
+    num1 = int(input("Please enter the first number: "))
+    num2 = int(input("Please enter the second number: "))
+    total = num1 + num2
+    print(total)
+
+AddNumbers()
